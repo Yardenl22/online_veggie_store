6229edabbd4bdf96035b3f8ed7a6e0a0c277cc47
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 15:44:50 +0200

    added generic api adapter

---
+++ frontend/src/app/services/api-adapter.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiAdapterService {
+  private baseUrl = 'http://localhost:8080'
+
+  constructor(private http: HttpClient) { }
+
+  get<T>(endpoint: string): Observable<T> {
+    return this.http.get<T>(`${this.baseUrl}${endpoint}`);
+  }
+
+  post<T>(endpoint: string, body: any): Observable<T> {
+    return this.http.post<T>(`${this.baseUrl}${endpoint}`, body);
+  }
+
+}
