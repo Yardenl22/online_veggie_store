114b3071c99e4e56feb01ec3418d1fef3918369c
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 15:43:11 +0200

    setup global layout

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { StoreComponent } from './components/store/store.component';
+
+export const routes: Routes = [
+  { path: '', component: StoreComponent },
+];
