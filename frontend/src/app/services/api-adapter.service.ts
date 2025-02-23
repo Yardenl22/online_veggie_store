2099a50febf81f2298f74d5911c910e2b8f261e1
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 21:15:44 +0200

    added env config file

--- frontend/src/app/services/api-adapter.service.ts
+++ frontend/src/app/services/api-adapter.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {environment} from '../../../enviorments';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ApiAdapterService {
-  private baseUrl = 'http://localhost:8080'
+  private baseUrl = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
