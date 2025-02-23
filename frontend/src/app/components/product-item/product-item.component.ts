537fc5efe4dc53247174bef7d223368201e281bf
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 15:47:58 +0200

    added product item component and models

---
+++ frontend/src/app/components/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import {CommonModule} from '@angular/common';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Product} from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.css'
+})
+export class ProductItemComponent {
+  @Input() product!: Product;
+  @Output() add = new EventEmitter<Product>();
+  @Output() remove = new EventEmitter<Product>();
+
+  onAdd() {
+    this.add.emit(this.product);
+  }
+
+  onRemove() {
+    this.remove.emit(this.product);
+  }
+
+}
