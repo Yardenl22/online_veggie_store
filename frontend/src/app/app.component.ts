525dc3240e0d1ea7a78e30064d36ee0c2c2999da
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 15:52:44 +0200

    --allow-empty

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ShoppingCartService} from './services/shopping-cart.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'online-veggie-store';
+  constructor(public shoppingCartService: ShoppingCartService) {}
+
+  get totalItems(): number {
+    return this.shoppingCartService.getTotalItems();
+  }
 }
