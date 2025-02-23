12417d900d5c5d3e87d88287c66c675ecd732b99
Yardenl22 <Yardenl227@gmail.com>
2025-02-23 20:20:10 +0200

    added styles to shopping-cart component

--- frontend/src/app/components/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,13 +1,18 @@
-import {CommonModule} from '@angular/common'
-import {Component, Signal} from '@angular/core';
-import {Product} from '../../models/product.model';
-import {ShoppingCartItem} from '../../models/shopping-cart.model';
-import {ShoppingCartService} from '../../services/shopping-cart.service';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { Component, Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../models/product.model';
+import { ShoppingCartItem } from '../../models/shopping-cart.model';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
+
+import { TableModule } from 'primeng/table';
+import {Panel} from 'primeng/panel';
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, TableModule, ButtonModule, Panel],
   templateUrl: './shopping-cart.component.html',
   styleUrl: './shopping-cart.component.css'
 })
